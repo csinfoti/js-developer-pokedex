a5b43908efd339659bbe0a3ef857e618eb078a26
csinfoti <cs.infoti@gmail.com>
2023-09-25 17:15:26 -0300

    Adição de mofificacao CS

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,9 @@
 
+
+
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
+function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
@@ -9,7 +11,8 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
-    pokemon.types = types
+
+    pokemon.types = types 
     pokemon.type = type
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
@@ -24,12 +27,15 @@ pokeApi.getPokemonDetail = (pokemon) => {
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
+    return fetch(url) //requisição para o servidor
+        .then((response) => response.json()) //converter a response para json
+        .then((jsonBody) => jsonBody.results) //criamos uma lista com o json obtido
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) //tranformar a primeira lista em lista de promises de detalhes do pokemon ja convertido em json
+        .then((detailRequests) => Promise.all(detailRequests)) //aguarda a realização e o término de todas as requests
         .then((pokemonsDetails) => pokemonsDetails)
+
+
 }
+
